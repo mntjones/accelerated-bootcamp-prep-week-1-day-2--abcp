a73fb91c2e53f5f24448e36f1f2d68b706d0ab36
Monica Tamaru Jones <mntamaru@gmail.com>
2018-01-14 17:36:08 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -1,25 +1,31 @@
 function addElementToBeginningOfArray(array, element) {
  // add the element to the beginning of the array without mutating the array
+  var newArray = [element, ...array]
+  return newArray
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   // add element to the beginning of the array by mutating the array
+  array.unshift(element)
+  return array
 }
 
 function addElementToEndOfArray(array, element) {
-
+  var newArray = [...array, element]
+  return newArray
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+  array.push(element)
+  return array
 }
 
 function accessElementInArray(array, index) {
-
+  return array[index]
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-
+  
 }
 
 function removeElementFromBeginningOfArray(array) {
