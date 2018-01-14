0ca03cbe1ad37b68ff1b4f8e04dc59470d896f18
Monica Tamaru Jones <mntamaru@gmail.com>
2018-01-14 18:08:14 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -34,7 +34,8 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-
+  array.shift()
+  return array
 }
 
 function removeElementFromEndOfArray(array) {
